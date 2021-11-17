00737f116adc5d400df0ffa161c3900dc665571f
Artem-PC <talanov_artem@ukr.net>
2021-11-18 00:20:06 +0200

    start

---
+++ src/components/Panel.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="panel">
+  <router-link class="home"
+               to="/"
+               exact
+               active-class="active"
+               tag="button"
+  >
+    <img src="../assets/icons/home.svg" alt="home">
+  </router-link>
+  <button class="gas-station" @click="showStations()">
+    <img src="../assets/icons/fuel.svg" alt="fuel">
+  </button>
+  <button class="qr-code" @click="getQr()">
+    <img src="../assets/icons/qr.svg" alt="qr" class="qr">
+  </button>
+  <button class="marker" @click="showMarker()">
+    <img src="../assets/icons/poiner.svg" alt="pointer">
+  </button>
+  <button class="more" @click="showMore()">
+    <span class="sircle"></span><span class="sircle"></span><span class="sircle"></span>
+  </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+  methods: {
+    showStations() {
+      console.log('Gas stations');
+    },
+
+    getQr() {
+      console.log("QR-code");
+    },
+
+    showMore() {
+      console.log("More");
+    },
+
+    showMarker() {
+      console.log("Marker");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .panel {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-around;
+    position: fixed;
+    max-width: 400px;
+    border-radius: 25px ;
+    height: 50px;
+    margin: auto;
+    bottom: 20px;
+    left: 20px;
+    right: 20px;
+    padding: 10px;
+    background-color: rgba(255, 255, 255, 0.3);
+    box-shadow: 0px 5px 6px -3px grey;
+    button {
+      width: 36px;
+      height: 36px;
+      border: snow;
+      background-color: rgba(255,255,255,0);
+      img {
+        width: 100%;
+      }
+      &.qr-code {
+        position: relative;
+        top: -100%;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        padding: 12px;
+        background-color: #43B2E7;
+      }
+      .sircle {
+        vertical-align: middle;
+        display: inline-block;
+        width: 4px;
+        height: 4px;
+        border: 1px solid gray;
+        border-radius: 50%;
+        margin-right: 2px;
+      }
+    }
+  }
+</style>
---
+++ src/views/Wash.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home">
+    <button class="list" @click="showList()">
+      <img src="../assets/icons/list.svg" alt="list">
+    </button>
+    <button class="to-me" @click="showPosition()">
+      <img src="../assets/icons/toMe.svg" alt="toMe">
+    </button>
+    <panel msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Panel from '@/components/Panel.vue'
+
+export default {
+  name: 'Wash',
+  components: {
+    Panel
+  },
+  methods: {
+    showList() {},
+    showPosition() {
+      console.log("you are here");
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .home {
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url(../assets/images/map.png);
+    background-repeat: no-repeat;
+    -webkit-background-size: cover;
+    background-size: cover;
+    .list {
+      position: fixed;
+      top: 30px;
+      left: 30px;
+      box-sizing: border-box;
+      width: 55px;
+      height: 55px;
+      padding: 15px 14px;
+      border: none;
+      box-shadow: 0 5px 10px -5px grey;
+      border-radius: 50%;
+      background-color: #F6F8F7;
+      img {
+        width: 100%;
+      }
+    }
+    .to-me {
+      position: fixed;
+      bottom: 120px;
+      right: 20px;
+      box-sizing: border-box;
+      width: 45px;
+      height: 45px;
+      border: none;
+      border-radius: 50%;
+      background-color: #F6F8F7;
+      box-shadow: 0 5px 10px -5px grey;
+
+    }
+  }
+</style>
